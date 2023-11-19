a524eeb3f9b3d366cc2dae2c6c7a945e9c710534
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-11-19 21:54:59 +0200

    terminal-like portfolio

--- JS/main.js
+++ JS/main.js
@@ -48,7 +48,7 @@ social =[
 "    ",
 '<FONT COLOR="#B89076">GitHub</font>        GitHub/RUKUNDO-Prince ',
 '<FONT COLOR="#B89076">LinkedIn</font>      LinkedIn/RUKUNDO Prince',
-'<FONT COLOR="#B89076">Twitter</font>       Twitter/_kevinishimwe',
+// '<FONT COLOR="#B89076">Twitter</font>       Twitter/_kevinishimwe',
 // '<FONT COLOR="#B89076">instagram</font>     Instagram/ishi_kevin',
 // '<FONT COLOR="#B89076">facebook</font>      Facebook/Ish kevin',
 '  ',];
