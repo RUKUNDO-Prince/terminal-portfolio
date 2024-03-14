2c164e975ffbfc4f2e88fb2cd5e58c9d1eef3adc
RUKUNDO Princde <rukundoprince951@gmail.com>
2024-03-14 20:41:45 +0200

    updating the terminalportfolio

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
-function $(elid) {
-  return document.getElementById(elid);
-}
+const about = document.querySelector("#about");
+const contact = document.querySelector("#contact");
+const aboutContent = document.querySelector("#about-content");
+const contactContent = document.querySelector("#contact-content");
 
-var cursor;
-window.onload = init;
+about.addEventListener("click", () => {
+  const aboutBox = new WinBox({
+    title: "About Me",
+    // modal: true,
+    width: "400px",
+    height: "400px",
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50,
+    mount: aboutContent,
+    onfocus: function () {
+      this.setBackground("var(--text-color)");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
 
-function init() {
-cursor = $("cursor");
-cursor.style.left = "0px";
-}
-
-function nl2br(txt) {
-return txt.replace(/\n/g, '');
-}
-
-function typeIt(from, e) {
-e = e || window.event;
-var w = $("typer");
-var tw = from.value;
-if (!pw){
-  w.innerHTML = nl2br(tw);
-}
-}
-
-function moveIt(count, e) {
-e = e || window.event;
-var keycode = e.keyCode || e.which;
-if (keycode == 37 && parseInt(cursor.style.left) >= (0 - ((count - 1) * 10))) {
-  cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
-} else if (keycode == 39 && (parseInt(cursor.style.left) + 10) <= 0) {
-  cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
-}
-}
-
-function alert(txt) {
-console.log(txt);
-}
+contact.addEventListener("click", () => {
+  const contactBox = new WinBox({
+    title: "Contact Me",
+    width: "400px",
+    height: "400px",
+    top: 100,
+    right: 50,
+    bottom: 50,
+    left: 150,
+    mount: contactContent,
+    onfocus: function () {
+      this.setBackground("var(--text-color)");
+    },
+    onblur: function () {
+      this.setBackground("#777");
+    },
+  });
+});
